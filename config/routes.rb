3347da8c137fe505afdc38f74df97f026d690485
shi-n-ya411 <180539615+shi-n-ya411@users.noreply.github.com>
2024-10-29 14:40:38 +0000

    [Finish] 4-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'show/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 
